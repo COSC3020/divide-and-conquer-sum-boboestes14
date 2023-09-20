ac9786e7bbcea66028a934f514751a0464fd4227
boboestes14 <143558849+boboestes14@users.noreply.github.com>
2023-09-20 22:32:32 +0000

    commit

--- code.js
+++ code.js
@@ -1,3 +1,24 @@
 function divideAndConquerSum(a) {
-    return a;
+    if(a.length <= 1){
+        return a[0];
+    }
+
+    split = Math.floor((a.length-1)/3);
+    ans = 0;
+    
+    b = divideAndConquerSum(a.slice(0,split+1));
+    ans = ans + b[0];
+    if((a.length/3)-split > .5){
+        c = divideAndConquerSum(a.slice(split+1,split*2+2));
+            ans = ans + c[0];
+    }
+
+    if((a.length/3)-split == 0){
+        d = divideAndConquerSum(a.slice(split*2+2,a.length));
+        ans = ans + d[0];
+    }
+    
+    return ans;
+    
+        
 }
